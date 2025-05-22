8f0fb3a48b7f62166485707f1685faa09575fa5e
gsunkersett <gaurangs@hotmail.com>
2025-05-21 20:05:59 -0700

    added db

--- api/src/functions/signups.js
+++ api/src/functions/signups.js
@@ -1,9 +1,56 @@
 const { app } = require('@azure/functions');
+const { Client } = require("pg");
+
 
 app.http('signups', {
     methods: ['GET', 'POST'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
         return { body: `Hello, from the API!` };
+    }
+});
+
+app.http('saveemail', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        let email = request.query.get('email');
+        console.log(`Received email: ${email}`);
+
+    if (!email) {
+        return {
+            status: 400,
+            body: "Email parameter is required."
+        };
+
+    }
+
+    const client = new Client({
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+    });
+
+    try {
+        await client.connect();
+        const query = "INSERT INTO emails (email) VALUES ($1)";
+        await client.query(query, [email]);
+
+        return{
+            status: 200,
+            body: `Email ${email} signed up successfully.`
+        };
+    } catch (error) {
+        context.log(`Database error: ${error.message}`);
+        return {
+            status: 500,
+            body: "Internal Server Error."
+        };
+    } finally {
+        await client.end();
+    }
+
     }
 });
